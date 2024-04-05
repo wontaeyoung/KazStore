fc255cc7e1f9079fdb1c6de2355a64b1c2279851
Taeyoung Won <hexter1994@gmail.com>
2024-04-05 16:46:53 +0900

    Initial Commit

---
+++ KazStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KazStore
+//
+//  Created by 원태영 on 4/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
