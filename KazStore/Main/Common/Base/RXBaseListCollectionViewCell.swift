e985baf5a9d605076c235c0cf57c89da951e6fbc
Taeyoung Won <hexter1994@gmail.com>
2024-04-05 18:38:22 +0900

    add: Base UI 클래스, 프로토콜 추가

---
+++ KazStore/Main/Common/Base/RXBaseListCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  RxBaseCollectionViewListCell.swift
+//  KazStore
+//
+//  Created by 원태영 on 4/5/24.
+//
+
+import UIKit
+import RxSwift
+
+class RxBaseCollectionViewListCell: UICollectionViewListCell {
+  
+  class var identifier: String {
+    return self.description()
+  }
+  
+  // MARK: - Property
+  var disposeBag = DisposeBag()
+  
+  // MARK: - Initializer
+  override init(frame: CGRect) {
+    
+    super.init(frame: frame)
+    
+    backgroundColor = .clear
+    contentView.backgroundColor = .clear
+    
+    setHierarchy()
+    setConstraint()
+    setAttribute()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycle
+  func setHierarchy() { }
+  func setConstraint() { }
+  func setAttribute() { }
+}
