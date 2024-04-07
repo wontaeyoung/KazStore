ad696caf6f17e4ad42edf11f359412498bfc823d
Taeyoung Won <hexter1994@gmail.com>
2024-04-07 22:40:03 +0900

    feat: App 모델 Repository 클래스 구현

---
+++ KazStore/Repository/AppRepository.swift
@@ -0,0 +1,25 @@
+//
+//  AppRepository.swift
+//  KazStore
+//
+//  Created by 원태영 on 4/5/24.
+//
+
+import RxSwift
+
+final class AppRepository {
+  
+  private let networkService: NetworkService
+  
+  init(networkService: NetworkService) {
+    self.networkService = networkService
+  }
+  
+  func fetch(query: String) -> Single<[App]> {
+    let router: ItunesAPIRouter = .app(query: query, limit: 10)
+    let response: Single<AppResponse> = networkService.callRequest(router: router)
+    
+    return response
+      .map { $0.results }
+  }
+}
