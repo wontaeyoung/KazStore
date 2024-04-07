31955232ed2c7defb4cc95208bae2327a2e0221c
Taeyoung Won <hexter1994@gmail.com>
2024-04-07 22:39:34 +0900

    add: Base 테이블 셀 클래스 추가

---
+++ KazStore/Main/Common/Base/RxBaseTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  RxBaseTableViewCell.swift
+//  KazStore
+//
+//  Created by 원태영 on 4/7/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class RxBaseTableViewCell: UITableViewCell {
+  
+  class var identifier: String {
+    return self.description()
+  }
+  
+  // MARK: - Property
+  var disposeBag = DisposeBag()
+  
+  // MARK: - Initializer
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    backgroundColor = .clear
+    contentView.backgroundColor = .clear
+    
+    setHierarchy()
+    setConstraint()
+    setAttribute()
+    bind()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycle
+  func setHierarchy() { }
+  func setConstraint() { }
+  func setAttribute() { }
+  func bind() { }
+}
