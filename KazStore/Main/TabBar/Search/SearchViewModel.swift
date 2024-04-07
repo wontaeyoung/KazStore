0eb5b6a9fc7c1d249e0a8d336e843578f28d84af
Taeyoung Won <hexter1994@gmail.com>
2024-04-07 22:41:36 +0900

    feat: 검색어 네트워크 요청 바인딩

--- KazStore/Main/TabBar/Search/SearchViewModel.swift
+++ KazStore/Main/TabBar/Search/SearchViewModel.swift
@@ -12,21 +12,43 @@ final class SearchViewModel: ViewModel {
   
   // MARK: - I / O
   struct Input {
-    
+    let searchEvent: PublishRelay<String>
   }
   
   struct Output {
-    
+    let apps: Driver<[App]>
   }
   
   // MARK: - Property
   let disposeBag = DisposeBag()
   weak var coordinator: SearchCoordinator?
+  private let appRepository: AppRepository
+  
+  // MARK: - Initializer
+  init(appRepository: AppRepository) {
+    self.appRepository = appRepository
+  }
   
   // MARK: - Method
   func transform(input: Input) -> Output {
     
-    return Output()
+    let apps = BehaviorRelay<[App]>(value: [])
+    
+    input.searchEvent
+      .thread(.global)
+      .withUnretained(self)
+      .flatMap { owner, query in
+        return owner.appRepository.fetch(query: query)
+          .catch {
+            LogManager.shared.log(with: $0, to: .network)
+            owner.coordinator?.showErrorAlert(error: $0)
+            return .just([])
+          }
+      }
+      .bind(to: apps)
+      .disposed(by: disposeBag)
+    
+    return Output(apps: apps.asDriver())
   }
 }
 
